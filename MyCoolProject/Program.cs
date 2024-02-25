eaa0fbbf60e5cd472cda754feed208794b69764c
feel <fosiuslegend@gmail.com>
2024-02-25 20:32:32 +0300

    Добавьте файлы проекта.

---
+++ MyCoolProject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCoolProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1000;
+            while(i != 0)
+            {
+                i--;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
